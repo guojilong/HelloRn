35c6ae51db19a08f78b2a72044cf9e046fea586f
guojilong <guo245296059@gmail.com>
2017-02-04 11:21:13 +0800

    add android index'

--- index.android.js
+++ index.android.js
@@ -11,16 +11,114 @@ import {
   Text,
   View,
   Image,
+  ListView
 } from 'react-native';
 
+
+var REQUEST_URL= 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+
 export default class HelloRn extends Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+     dataSource: new ListView.DataSource({
+      rowHasChanged:(row1,row2)=>row1!== row2,
+     }
+      ),
+
+     loaded: false,
+  };
+
+  this.fetchData = this.fetchData.bind(this); 
+}
  
-  render() {
-    var MOCKED_MOVIES_DATA=[{title: '标题', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}}
-];
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData(){
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+
+      this.setState({
+
+
+      dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+
+      loaded: true,
+
+        });
+    });
     
-    var movie = MOCKED_MOVIES_DATA[0];
+  }
+
+
+  render() {
+//     var MOCKED_MOVIES_DATA=[{title: '标题', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}}
+// ];
+    
+//     var movie = MOCKED_MOVIES_DATA[0];
+//     return (
+//       <View style={styles.container}>
+     
+    
+//       <Image source={{uri: movie.posters.thumbnail}}
+//       style={styles.thumbnail} />
+
+//       <View style={styles.rightContainer}>
+
+//       <Text style={styles.title}>{movie.title}</Text>
+//       <Text style={styles.year}>{movie.year}</Text>
+
+//       </View>
+//       </View>
+    
+     
+//     );
+
+    if(!this.state.loaded){
+      return this.renderLoadingView();
+    }
+
+    // var movie=this.state.movies[0];
+
+    // return this.renderMovie(movie);
+
     return (
+
+      <ListView
+      dataSource={this.state.dataSource}
+      renderRow={this.renderMovie}
+      style={styles.listView}
+/>
+
+      );
+  }
+
+
+
+  renderLoadingView(){
+    return(
+    <View style={styles.container}>
+      <Text>
+
+      正在加载电影数据......
+      </Text>
+
+    </View>
+
+
+      );
+
+  }
+
+
+  renderMovie(movie){
+        return (
       <View style={styles.container}>
      
     
@@ -28,26 +126,24 @@ export default class HelloRn extends Component {
       style={styles.thumbnail} />
 
       <View style={styles.rightContainer}>
-
+ 
       <Text style={styles.title}>{movie.title}</Text>
       <Text style={styles.year}>{movie.year}</Text>
 
-      <Text>hello rn</Text>
       </View>
       </View>
-    
-     
-    );
+      );
+
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
   },
   welcome: {
     fontSize: 20,
@@ -59,21 +155,26 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-  // thumbnail:{
-  //   width: 53,
-  //   height: 81,
-  // },
+  thumbnail:{
+    width: 53,
+    height: 81,
+  },
   rightContainer:{
     flex: 1,
   },
   title:{
     fontSize: 20,
     marginBottom: 8,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   year:{
-    textAlign: 'center'
+    textAlign: 'center',
   },
+  listView:{
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: '#F5FCFF'
+  }
  
 });
 
